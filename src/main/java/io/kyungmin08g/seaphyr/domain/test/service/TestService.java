907e6739923187d55cab5d1750afe0ec62de5d62
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-20 11:34:10 +0900

    [FEAT] test 서비스 구현

---
+++ src/main/java/io/kyungmin08g/seaphyr/domain/test/service/TestService.java
@@ -0,0 +1,11 @@
+package io.kyungmin08g.seaphyr.domain.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+  public void print() {
+    System.out.println("Hello, World!");
+  }
+}
